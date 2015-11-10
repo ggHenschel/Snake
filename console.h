baed57042a4f1edeb86eacc3b87d7679d806585e
Guilherme Gustavo Henschel <guilherme.g.henschel@gmail.com>
2015-11-10 09:48:13 -0200

    Version 2015.11.10.01

---
+++ console.h
@@ -0,0 +1,9 @@
+/* console.h */
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+int kbhit (void);
+int getch ();
+
+#endif
---
+++ menu.h
@@ -0,0 +1,24 @@
+// PONTIFICA UNIVERSIDADE CATOLICA DO PARANA
+// ESCOLA POLITECNICA
+// CIENCIA DA COMPUTACAO
+// PROGRAMACAO IMPERATIVA
+//
+// GUILHERME GUSTAVO HENSCHEL
+// RAFAEL DE SOUZA TEIXEIRA
+//
+// IMPLEMENTACAO MULTIPLATAFORMA DE SNAKE
+// EM C UTILIZANDO RECURSOS GRAFICOS DO
+// TERMINAL OU PROMTCMD
+//
+// CURITIBA 2015
+
+#ifndef menu_h
+#define menu_h
+#include <stdio.h>
+#include "tela.h"
+
+void menuStart(char telaInicial[ALTURA][LARGURA], char path[]);
+void gameOver(char tela[ALTURA][LARGURA], char path[]);
+
+
+#endif
